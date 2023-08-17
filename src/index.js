595e77efc094e61308f9da79ac51817126106240
ilginokdem <okdemilgin@gmail.com>
2023-08-17 15:49:00 +0300

    S5G3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './index.less'
 import './bileşenler/haber/haberler.js'
 import './bileşenler/menu/menu.js'
+import "./index.less";
+import "./bileşenler/menu/menu.js";
+import "./bileşenler/haber/haberler.js";
